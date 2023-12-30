de0ae1168cc629923c60d74732627cdd9d6791f8
Adrian Talbot <adrian.r.talbot@gmail.com>
2023-12-30 21:22:03 +0000

    Initial Commit

---
+++ Factorise/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Factorise
+//
+//  Created by Adrian Talbot on 30/12/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
